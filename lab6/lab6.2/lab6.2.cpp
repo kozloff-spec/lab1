fad4d41b7a0368d24bbfeba60131109cbcf9e07e
Alexandr Kozlov <epikbro9@gmail.com>
2021-10-27 10:50:03 +0300

    1

--- lab6/lab6.2/lab6.2.cpp
+++ lab6/lab6.2/lab6.2.cpp
@@ -9,23 +9,26 @@ int main()
     int correct = 0;
     int xmax = 9;
     int ymax = 9;
-    srand(time(0));
-    int random_number = rand();
-    printf(" ");
-    scanf_s("%d",&n);
-    int nn = n;
-    for (nn; nn > 0; nn--) {
-        int x = (rand() % xmax) + 1;
-        int y = (rand() % ymax) +1;
+    while (true) {
+        srand(time(0));
+        int random_number = rand();
+        printf("Сколько примеров вы хотите ? - ");
+        scanf_s("%d", &n);
+        int nn = n;
+        for (nn; nn > 0; nn--) {
+            int x = (rand() % xmax) + 1;
+            int y = (rand() % ymax) + 1;
 
-        printf("Решите пример %d * %d = ", x,y);
-        int answ;
-        scanf_s("%d", &answ); 
-        if (answ == (x * y)) {
-            correct++;
+            printf("Решите пример %d * %d = ", x, y);
+            int answ;
+            scanf_s("%d", &answ);
+            if (answ == (x * y)) {
+                correct++;
+            }
         }
+        printf("ваша оценка %d из %d \n\n\n", correct, n);
     }
-    printf("ваша оценка %d из %d", correct,n);
+
 }
 
 
